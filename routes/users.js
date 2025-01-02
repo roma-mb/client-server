e00c6d6c0fc16337a8fc05a67a34f0799ccd049d
Romário Melo <romario.melo.barreiras@gmail.com>
2025-01-02 07:12:17 -0300

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+let restify = require('restify-clients');
+let assert = require('assert');
+let express = require('express');
+let router = express.Router();
+
+let client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
+
+/* GET users listing. */
+router.get('/', function(request, response, next) {
+  client.get('/users', function(error, clientRequest, clientResponse, obj) {
+    assert.ifError(error);
+    response.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+module.exports = router;
